37bcc2e827f551a094a55f1c28137daadd858109
Forrest Oliphant <forrest@sembiki.com>
2013-10-06 22:00:19 +0200

    pan and zoom attributes on the-graph

---
+++ the-graph-util/the-graph-util.js
@@ -0,0 +1,34 @@
+(function (){
+
+var util = {};
+window.theGraph = util;
+
+util.transform = function (el, x, y, scale) {
+  if (scale === undefined) { scale = 1; }
+  var move = "translate3d("+x+"px,"+y+"px,0px) scale("+scale+")";
+
+  // TODO prefix test
+  el.style.webkitTransform = move;
+  el.style.MozTransform = move;
+  el.style.msTransform = move;
+  el.style.OTransform = move;
+  el.style.transform = move;
+  return el;
+};
+
+// util.debounce = function(func, wait, immediate) {
+//   var timeout;
+//   return function() {
+//     var context = this, args = arguments;
+//     var later = function() {
+//       timeout = null;
+//       if (!immediate) func.apply(context, args);
+//     };
+//     var callNow = immediate && !timeout;
+//     clearTimeout(timeout);
+//     timeout = setTimeout(later, wait);
+//     if (callNow) func.apply(context, args);
+//   };
+// };
+
+})();
