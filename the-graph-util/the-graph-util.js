e80351afc8e63003440c2e7164aa5d71599169ff
Forrest Oliphant <forrest@sembiki.com>
2013-10-24 14:59:46 +0300

    round all transforms #24

--- the-graph-util/the-graph-util.js
+++ the-graph-util/the-graph-util.js
@@ -5,8 +5,10 @@ window.theGraph = util;
 
 util.transform = function (el, x, y, scale) {
   if (scale === undefined) { scale = 1; }
+  x = Math.round(x);
+  y = Math.round(y);
   var move = "translate3d("+x+"px,"+y+"px,0px) scale("+scale+")";
-
+  
   // TODO prefix test
   el.style.webkitTransform = move;
   el.style.MozTransform = move;
@@ -16,19 +18,5 @@ util.transform = function (el, x, y, scale) {
   return el;
 };
 
-// util.debounce = function(func, wait, immediate) {
-//   var timeout;
-//   return function() {
-//     var context = this, args = arguments;
-//     var later = function() {
-//       timeout = null;
-//       if (!immediate) func.apply(context, args);
-//     };
-//     var callNow = immediate && !timeout;
-//     clearTimeout(timeout);
-//     timeout = setTimeout(later, wait);
-//     if (callNow) func.apply(context, args);
-//   };
-// };
 
 })();
