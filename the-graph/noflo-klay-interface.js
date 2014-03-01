65b2b8295f83bad03ccdcaf8842cbfeafdd442cb
Forrest Oliphant <forrest@sembiki.com>
2014-03-01 10:57:58 +0200

    autolayout graph inports and outports as well #90. we should find a way to force them to first/last layer.

--- the-graph/noflo-klay-interface.js
+++ the-graph/noflo-klay-interface.js
@@ -13,25 +13,64 @@
   };
 
   // Encode the original NoFlo graph as a KGraph (KIELER Graph) JSON
-  var toKieler = function (graph) {
-    var kGraph = {id: 'root',
-                  children: [], 
-                  edges: []};
+  var toKieler = function (graph, portInfo) {
+    var kGraph = {
+      id: 'root',
+      children: [], 
+      edges: []
+    };
 
     // Encode nodes
     var nodes = graph.nodes;
     var idx = {};
     var countIdx = 0;
-    nodes.map(function (node) {
-      kGraph.children.push({id: node.id, 
-                            labels: [{text: node.metadata.label}],
-                            // Math.max(72, 8*node.metadata.label.length),
-                            width: 72, 
-                            height: 108,
-                            ports: []
-                           });
+    var nodeChildren = nodes.map(function (node) {
+      var kChild = {
+        id: node.id,
+        labels: [{text: node.metadata.label}],
+        width: 72, 
+        height: 108,
+        ports: []
+      };
       idx[node.id] = countIdx++;
+      return kChild;
     });
+
+    // Graph i/o to kGraph nodes
+    var inports = graph.inports;
+    var inportsKeys = Object.keys(inports);
+    var inportChildren = inportsKeys.map(function(key){
+      var inport = inports[key];
+      var tempId = "inport:::"+key;
+      var kChild = {
+        id: tempId, 
+        labels: [{text: key}],
+        width: 72, 
+        height: 108,
+        ports: []
+      };
+      idx[tempId] = countIdx++;
+      return kChild;
+    });
+    var outports = graph.outports;
+    var outportsKeys = Object.keys(outports);
+    var outportChildren = outportsKeys.map(function(key){
+      var outport = outports[key];
+      var tempId = "outport:::"+key;
+      var kChild = {
+        id: tempId, 
+        labels: [{text: key}],
+        width: 72, 
+        height: 108,
+        ports: []
+      };
+      idx[tempId] = countIdx++;
+      return kChild;
+    });
+
+    // Combine nodes, inports, outports to one array
+    kGraph.children = nodeChildren.concat(inportChildren, outportChildren);
+
 
     // Encode edges (together with ports on both edges and already encoded nodes)
     var currentEdge = 0;
@@ -53,26 +92,66 @@
                          targetPort: target + '_' + targetPort
                         });
 
-      // Complete nodes encoding adding ports to them
-      var ports = kGraph.children[idx[source]].ports;
-      var port = {id: source + '_' + sourcePort, 
-                  width: 10, 
-                  height: 10, 
-                  properties: {'de.cau.cs.kieler.portSide': 'EAST'}};
-      if (ports.indexOf(port) < 0) {
-        ports.push(port);
-      }
+      // Can use portInfo here
 
-      ports = kGraph.children[idx[target]].ports;
-      port = {id: target + '_' + targetPort, 
-                  width: 10, 
-                  height: 10, 
-                  properties: {'de.cau.cs.kieler.portSide': 'WEST'}};
-      if (ports.indexOf(port) < 0) {
-        ports.push(port);
-      }
+      // // Complete nodes encoding adding ports to them
+      // var ports = kGraph.children[idx[source]].ports;
+      // var port = {id: source + '_' + sourcePort, 
+      //             width: 10, 
+      //             height: 10, 
+      //             properties: {'de.cau.cs.kieler.portSide': 'EAST'}};
+      // if (ports.indexOf(port) < 0) { 
+      //   ports.push(port);
+      // }
+
+      // ports = kGraph.children[idx[target]].ports;
+      // port = {id: target + '_' + targetPort, 
+      //             width: 10, 
+      //             height: 10, 
+      //             properties: {'de.cau.cs.kieler.portSide': 'WEST'}};
+      // if (ports.indexOf(port) < 0) {
+      //   ports.push(port);
+      // }
       
     });
+    
+    // Graph i/o to kGraph edges
+    var inportEdges = inportsKeys.map(function (key) {
+      var inport = inports[key];
+      var source = "inport:::"+key;
+      var sourcePort = key;
+      var target = inport.process;
+      var targetPort = inport.port;
+      var inportEdge = {
+        id: 'e' + currentEdge++,
+        source: source,
+        sourcePort: source + '_' + sourcePort,
+        target: target,
+        targetPort: target + '_' + targetPort
+      };
+      return inportEdge;
+    });
+    var outportEdges = outportsKeys.map(function (key) {
+      var outport = outports[key];
+      var source = outport.process;
+      var sourcePort = outport.port;
+      var target = "outport:::"+key;
+      var targetPort = key;
+      var outportEdge = {
+        id: 'e' + currentEdge++,
+        source: source,
+        sourcePort: source + '_' + sourcePort,
+        target: target,
+        targetPort: target + '_' + targetPort
+      };
+      return outportEdge;
+    });
+
+
+    // Combine edges, inports, outports to one array
+    kGraph.edges = kGraph.edges.concat(inportEdges, outportEdges);
+
+
 
     // FIXME: groups are not supported on KLayJS, uncomment the following lines
     // when it gets support
@@ -125,9 +204,9 @@
   };
 
   // Main interface for now: apply KLayJS layout algorithm and call the render
-  window.klay = function (graph, render) {
+  window.klay = function (graph, portInfo, render) {
     // Convert the NoFlo graph to KGraph
-    var kGraph = toKieler(graph);
+    var kGraph = toKieler(graph, portInfo);
     
     // Define some preset options to KLayJS
     var options = {"algorithm": "de.cau.cs.kieler.klay.layered",
@@ -140,6 +219,7 @@
                     render(layouted);
                   },
                   error: function (error) {
+                    // CAVEAT: this will catch errors in render callback
                     console.log("$klay.layout error:", error);
                   }});
   };
