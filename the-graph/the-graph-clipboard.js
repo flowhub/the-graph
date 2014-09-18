4ce6cf1812fc55be85f870c9167b3747b3d3adad
Forrest Oliphant <forrest@sembiki.com>
2014-09-18 13:40:32 +0300

    2 space #167

--- the-graph/the-graph-clipboard.js
+++ the-graph/the-graph-clipboard.js
@@ -3,72 +3,69 @@
  */
 
 (function (context) {
-    "use strict";
-    var TheGraph = context.TheGraph;
+  "use strict";
+  var TheGraph = context.TheGraph;
 
-    TheGraph.Clipboard = {};
-    var clipboardContent = {};
+  TheGraph.Clipboard = {};
+  var clipboardContent = {};
 
-    var cloneObject = function (obj) {
-        return JSON.parse(JSON.stringify(obj));
-    };
+  var cloneObject = function (obj) {
+    return JSON.parse(JSON.stringify(obj));
+  };
 
-    var makeNewId = function (label) {
-        var num = 60466176; // 36^5
-        num = Math.floor(Math.random() * num);
-        var id = label + '_' + num.toString(36);
-        return id;
-    };
+  var makeNewId = function (label) {
+    var num = 60466176; // 36^5
+    num = Math.floor(Math.random() * num);
+    var id = label + '_' + num.toString(36);
+    return id;
+  };
 
-    TheGraph.Clipboard.copy = function (graph, keys) {
-        //Duplicate all the nodes before putting them in clipboard
-        //this will make this work also with cut/Paste and once we
-        //decide if/how we will implement cross-document copy&paste will work there too
-        clipboardContent = {nodes:[],edges:[]};
-        var map = {};
-        for (var nodeKey in keys) {
-            var node = graph.getNode(keys[nodeKey]);
-            var newNode = cloneObject(node);
-            newNode.id = makeNewId(node.component);
-            clipboardContent.nodes.push(newNode);
-            map[node.id] = newNode.id;
-        }
-        for (var edgeKey in graph.edges) {
-            var edge = graph.edges[edgeKey];
-            var fromNode = edge.from.node;
-            var toNode = edge.to.node;
-            if (map.hasOwnProperty(fromNode) && map.hasOwnProperty(toNode)) {
-                var newEdge = cloneObject(edge);
-                newEdge.from.node = map[fromNode];
-                newEdge.to.node = map[toNode];
-                clipboardContent.edges.push(newEdge);
-            }
-        }
+  TheGraph.Clipboard.copy = function (graph, keys) {
+    //Duplicate all the nodes before putting them in clipboard
+    //this will make this work also with cut/Paste and once we
+    //decide if/how we will implement cross-document copy&paste will work there too
+    clipboardContent = {nodes:[],edges:[]};
+    var map = {};
+    for (var nodeKey in keys) {
+      var node = graph.getNode(keys[nodeKey]);
+      var newNode = cloneObject(node);
+      newNode.id = makeNewId(node.component);
+      clipboardContent.nodes.push(newNode);
+      map[node.id] = newNode.id;
+    }
+    for (var edgeKey in graph.edges) {
+      var edge = graph.edges[edgeKey];
+      var fromNode = edge.from.node;
+      var toNode = edge.to.node;
+      if (map.hasOwnProperty(fromNode) && map.hasOwnProperty(toNode)) {
+        var newEdge = cloneObject(edge);
+        newEdge.from.node = map[fromNode];
+        newEdge.to.node = map[toNode];
+        clipboardContent.edges.push(newEdge);
+      }
+    }
 
-    };
+  };
 
-    TheGraph.Clipboard.paste = function (graph) {
-        var map = {};
-        for (var nodeKey in clipboardContent.nodes) {
-            var node = clipboardContent.nodes[nodeKey];
-            var meta = cloneObject(node.metadata);
-            meta.x += 10;
-            meta.y += 10;
-            var newNode = graph.addNode(makeNewId(node.component), node.component, meta);
-            map[node.id] = newNode.id;
-        }
-        for (var edgeKey in clipboardContent.edges) {
-            var edge = clipboardContent.edges[edgeKey];
-            var fromNode = edge.from.node;
-            var toNode = edge.to.node;
-            var newEdgeMeta = cloneObject(edge.metadata);
-            // TODO check if we need to addEdgeIndex for addressable ports
-            graph.addEdge(map[fromNode], edge.from.port, map[toNode], edge.to.port, newEdgeMeta);
-        }
+  TheGraph.Clipboard.paste = function (graph) {
+    var map = {};
+    for (var nodeKey in clipboardContent.nodes) {
+      var node = clipboardContent.nodes[nodeKey];
+      var meta = cloneObject(node.metadata);
+      meta.x += 10;
+      meta.y += 10;
+      var newNode = graph.addNode(makeNewId(node.component), node.component, meta);
+      map[node.id] = newNode.id;
+    }
+    for (var edgeKey in clipboardContent.edges) {
+      var edge = clipboardContent.edges[edgeKey];
+      var fromNode = edge.from.node;
+      var toNode = edge.to.node;
+      var newEdgeMeta = cloneObject(edge.metadata);
+      // TODO check if we need to addEdgeIndex for addressable ports
+      graph.addEdge(map[fromNode], edge.from.port, map[toNode], edge.to.port, newEdgeMeta);
+    }
 
-    };
+  };
 
 })(this);
-
-
-
