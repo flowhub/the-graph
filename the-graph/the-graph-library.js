62b67fdb0d26f261d0a634ceb9a03a496a001f20
Jon Nordby <jononor@gmail.com>
2017-01-06 14:06:48 +0100

    the-graph-editor: Move component creation from graph out of Polymer element

--- the-graph/the-graph-library.js
+++ the-graph/the-graph-library.js
@@ -58,6 +58,91 @@ function mergeComponentDefinition(component, definition) {
   return definition;
 }
 
+function componentsFromGraph(fbpGraph) {
+  var components = [];
+
+  fbpGraph.nodes.forEach(function (node) {
+    var component = {
+      name: node.component,
+      icon: 'cog',
+      description: '',
+      inports: [],
+      outports: []
+    };
+
+    Object.keys(fbpGraph.inports).forEach(function (pub) {
+      var exported = fbpGraph.inports[pub];
+      if (exported.process === node.id) {
+        for (var i = 0; i < component.inports.length; i++) {
+          if (component.inports[i].name === exported.port) {
+            return;
+          }
+        }
+        component.inports.push({
+          name: exported.port,
+          type: 'all'
+        });
+      }
+    });
+    Object.keys(fbpGraph.outports).forEach(function (pub) {
+      var exported = fbpGraph.outports[pub];
+      if (exported.process === node.id) {
+        for (var i = 0; i < component.outports.length; i++) {
+          if (component.outports[i].name === exported.port) {
+            return;
+          }
+        }
+        component.outports.push({
+          name: exported.port,
+          type: 'all'
+        });
+      }
+    });
+    fbpGraph.initializers.forEach(function (iip) {
+      if (iip.to.node === node.id) {
+        for (var i = 0; i < component.inports.length; i++) {
+          if (component.inports[i].name === iip.to.port) {
+            return;
+          }
+        }
+        component.inports.push({
+          name: iip.to.port,
+          type: 'all'
+        });
+      }
+    });
+
+    fbpGraph.edges.forEach(function (edge) {
+      var i;
+      if (edge.from.node === node.id) {
+        for (i = 0; i < component.outports.length; i++) {
+          if (component.outports[i].name === edge.from.port) {
+            return;
+          }
+        }
+        component.outports.push({
+          name: edge.from.port,
+          type: 'all'
+        });
+      }
+      if (edge.to.node === node.id) {
+        for (i = 0; i < component.inports.length; i++) {
+          if (component.inports[i].name === edge.to.port) {
+            return;
+          }
+        }
+        component.inports.push({
+          name: edge.to.port,
+          type: 'all'
+        });
+      }
+    });
+    components.push(component);
+  });
+  return components;
+}
+
 module.exports = {
   mergeComponentDefinition: mergeComponentDefinition,
+  componentsFromGraph: componentsFromGraph,
 };
