8d19254abcd5c24c714761ab6369abca3f04a3a8
Jon Nordby <jononor@gmail.com>
2017-05-06 15:10:26 +0200

    TheGraph.merge(): Move to CommonJS module

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ if (typeof window !== 'undefined' && typeof window.Ease === 'undefined') {
 var g = { TheGraph: TheGraph };
 
 TheGraph.factories = require('./the-graph/factories.js');
+TheGraph.merge = require('./the-graph/merge.js');
 
 require("./the-graph/the-graph.js").register(g);
 require("./the-graph/the-graph-app.js").register(g);
--- the-graph/the-graph.js
+++ the-graph/the-graph.js
@@ -345,24 +345,6 @@ module.exports.register = function (context) {
       );
     }
   }));
-
-  // The `merge` function provides simple property merging.
-  TheGraph.merge = function(src, dest, overwrite) {
-    // Do nothing if neither are true objects.
-    if (Array.isArray(src) || Array.isArray(dest) || typeof src !== 'object' || typeof dest !== 'object')
-      return dest;
-
-    // Default overwriting of existing properties to false.
-    overwrite = overwrite || false;
-
-    for (var key in src) {
-      // Only copy properties, not functions.
-      if (typeof src[key] !== 'function' && (!dest[key] || overwrite))
-        dest[key] = src[key];
-    }
-
-    return dest;
-  };
   
   TheGraph.getOffset = function(domNode){
     var getElementOffset = function(element){
