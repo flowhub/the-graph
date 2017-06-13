4e051f2f221955c6988698225ca9ff394d4c11a4
Jon Nordby <jononor@gmail.com>
2017-06-13 19:34:34 +0200

    the-graph-nav: Also render view rectangle in React component

--- the-graph-nav/the-graph-nav.js
+++ the-graph-nav/the-graph-nav.js
@@ -1,4 +1,6 @@
 
+var thumb = require('../the-graph-thumb/the-graph-thumb.js');
+
 function calculateStyleFromTheme(theme) {
   var style = {};
   if (theme === "dark") {
@@ -87,14 +89,31 @@ function renderViewRectangle(context, viewrect, props) {
 }
 
 function renderThumbnailFromProps(context, props) {
-    console.log('nav.renderThumb() start', typeof canvas, this.props);
-    var style = TheGraph.thumb.styleFromTheme(props.theme);
-    style.width = props.width;
-    style.height = props.height;
-    style.nodeSize = props.nodeSize;
+    console.log('nav.renderThumb() start', props);
+    var style = {};
+    for (var name in props) {
+      style[name] = props[name];
+    }
+    style.graph = null;
     style.lineWidth = props.nodeLineWidth;
-    var info = TheGraph.thumb.render(context, props.graph, style);
+    var info = thumb.render(context, props.graph, style);
     console.log('nav.renderThumb() done', info);
+    return info;
+}
+function renderViewboxFromProps(context, viewbox, thumbInfo, props) {
+    console.log('nav.renderViewbox() start', props);
+    var style = {};
+    for (var name in props) {
+      style[name] = props[name];
+    }
+    style.graph = null;
+    style.scale = props.viewscale;
+    var thumbW = thumbInfo.rectangle[2];
+    var thumbH = thumbInfo.rectangle[3];
+    style.thumbscale = (thumbW>thumbH) ? props.width/thumbW : props.height/thumbH;
+    style.thumbrectangle = thumbInfo.rectangle;
+    var info = renderViewRectangle(context, viewbox, style);
+    console.log('nav.renderViewbox() done', info);
     return info;
 }
 
@@ -107,13 +126,15 @@ var Component = React.createClass({
       width: 200,
       height: 150,
       hidden: false, // FIXME: drop??
-      theme: 'dark',
       backgroundColor: "hsla(184, 8%, 75%, 0.9)",
       outsideFill: "hsla(0, 0%, 0%, 0.4)",
       nodeSize: 60,
       nodeLineWidth: 1,
-      border: '1px solid hsla(190, 100%, 80%, 0.4)',
-      borderStyle: 'dotted',
+      viewrectangle: [0, 0, 0, 0],
+      viewscale: 1.0,
+      viewBoxBorder: "hsla(190, 100%, 80%, 0.4)",
+      viewBoxBorder2: "hsla( 10,  60%, 32%, 0.3)",
+      viewBoxBorderStyle: 'dotted',
       graph: null, // NOTE: should not attach to events, that is responsibility of outer code
     };
   },
@@ -122,7 +143,6 @@ var Component = React.createClass({
     };
   },
   render: function() {
-    console.log('nav.render() start');
     var p = this.props;
     var thumbStyle = {
       position: 'absolute',
@@ -132,8 +152,9 @@ var Component = React.createClass({
     var wrapperStyle = {
       height: p.height,
       width: p.width,
-      border: p.border,
-      borderStyle: p.borderStyle,
+      overflow: "hidden",
+      cursor: "move",
+      backgroundColor: p.backgroundColor,
     };
     var thumbProps = {
       key: 'thumb',
@@ -142,23 +163,53 @@ var Component = React.createClass({
       height: p.height,
       style: thumbStyle,
     };
+    var viewboxCanvas = {
+      key: 'viewbox',
+      ref: this._refViewboxCanvas,
+      width: p.width,
+      height: p.height,
+      style: thumbStyle,
+    };
+    // FIXME: find better way to populate the props from render function
+    var viewboxDiv = {
+      key: 'viewboxdiv',
+      ref: this._refViewboxElement,
+      style: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: p.width,
+        height: p.height,
+        borderStyle: 'dotted',
+        borderWidth: 1,
+      },
+    };
     // Elements
     var d = React.DOM;
     return d.div( { style: wrapperStyle }, [
+      d.div( viewboxDiv ),
+      d.canvas( viewboxCanvas ),
       d.canvas( thumbProps ),
     ]);
   },
   _refThumbCanvas: function(canvas) {
-      // Store the DOM node, since we need that for actually rendering
       this._thumbContext = canvas.getContext('2d');
+  },
+  _refViewboxCanvas: function(canvas) {
+      this._viewboxContext = canvas.getContext('2d');
+  },
+  _refViewboxElement: function(el) {
+      this._viewboxElement = el;
   },
   componentDidUpdate: function() {
     console.log('nav componentDidUpdate');
-    renderThumbnailFromProps(this._thumbContext, this.props);
+    var t = renderThumbnailFromProps(this._thumbContext, this.props);
+    renderViewboxFromProps(this._viewboxContext, this._viewboxElement, t, this.props);
   },
   componentDidMount: function() {
     console.log('nav componentMount');
-    renderThumbnailFromProps(this._thumbContext, this.props);
+    var t = renderThumbnailFromProps(this._thumbContext, this.props);
+    renderViewboxFromProps(this._viewboxContext, this._viewboxElement, t, this.props);
   }
 });
 
